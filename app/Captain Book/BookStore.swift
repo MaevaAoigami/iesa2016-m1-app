7c2150daca3659ad43f57c3b07e13dce360ea772
Mastere 1 IT <mastere1it@iMacdeAutres6.sto-002.saint-augustin.in.iesanetwork.com>
2016-06-21 14:43:32 +0200

    Add model

---
+++ app/Captain Book/BookStore.swift
@@ -0,0 +1,13 @@
+//
+//  BookStore.swift
+//  Captain Book
+//
+//  Created by Mastere 1 IT on 21/06/2016.
+//  Copyright © 2016 Maeva Aoigami. All rights reserved.
+//
+
+import Foundation
+
+class BookStore {
+    
+}
---
+++ app/Captain Book/Captain Book/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  Captain Book
+//
+//  Created by Mastere 1 IT on 21/06/2016.
+//  Copyright © 2016 Maeva Aoigami. All rights reserved.
+//
+
+import Foundation
+
+class Book {
+    var title: String = "Title"
+    var author: String = "Author"
+    var description: String = "Description"
+}
